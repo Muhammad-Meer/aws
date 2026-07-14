5f8f7d4292725f179c4a99f30b312fe561d417fe
Muhammad Meer <sadbadbadbbadluck@gmail.com>
2026-07-14 11:27:26 +0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_5x95CEFhE',
-        userPoolClientId: '1n0dh8iebgdg4tcl17ale60c3i',
+        userPoolId: 'us-west-2_4L3KVmoW4',
+        userPoolClientId: '6s9k1lramqprr8fv84976adqj4',
         region: 'us-east-1'
     },
     api: {
