d90fd6f2c5ed33216a02449bdbdb6056d91a75ed
Muhammad Meer <sadbadbadbbadluck@gmail.com>
2026-07-14 11:29:18 +0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-west-2_4L3KVmoW4',
         userPoolClientId: '6s9k1lramqprr8fv84976adqj4',
-        region: 'us-east-1'
+        region: 'us-west-2'
     },
     api: {
         invokeUrl: '' // Will be updated after API Gateway is created
